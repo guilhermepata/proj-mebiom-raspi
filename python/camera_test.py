00a17863bf11d0405782e60c1ec4c30df12faf5a
guilhermepata-raspi <pataguilherme@gmail.com>
2020-11-06 15:28:32 +0000

    First commit woo btw the camera works ;)

---
+++ python/camera_test.py
@@ -0,0 +1,11 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera();
+
+camera.start_preview()
+camera.start_recording('/home/pi/Desktop/video_test.h264')
+sleep(60)
+camera.stop_recording()
+camera.stop_preview()
+print('done recording');
