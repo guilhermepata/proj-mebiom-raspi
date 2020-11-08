9412bb34871dec1cff61a467538e06186881e574
guilhermepata <pataguilherme@gmail.com>
2020-11-08 16:26:48 +0000

    Update object_recog_test.py
    Updated pre-requisites with link to h5 file

--- python/object_recog_test.py
+++ python/object_recog_test.py
@@ -1,12 +1,17 @@
 # pre-requisites:
-
+#
 # pip install tensorflow==1.13.1
 # pip install opencv-python
 # pip install keras==2.2.4
 # pip install numpy==1.16.1
 # pip install imageai --upgrade
+#
+# download this file into current directory:
+# https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/resnet50_coco_best_v2.0.1.h5
+#
+# code adapted from:
+# https://towardsdatascience.com/object-detection-with-10-lines-of-code-d6cb4d86f606
 
-# code from: https://towardsdatascience.com/object-detection-with-10-lines-of-code-d6cb4d86f606
 from imageai.Detection import ObjectDetection
 import os
 
